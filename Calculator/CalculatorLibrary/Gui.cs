91446564be3a98d633e5b040e02daafd313e1cbe
allweareisdustinthewind <tololo_ilya@web.de>
2026-05-03 13:51:20 +0200

    Finished

---
+++ Calculator/CalculatorLibrary/Gui.cs
@@ -0,0 +1,33 @@
+﻿namespace CalculatorLibrary;
+public class Gui
+{
+   public static void WriteColorText (string text, ConsoleColor colorFoderground, int colorBackground = -1)
+   {
+      ConsoleColor curColorForeground = Console.ForegroundColor;
+      ConsoleColor curColorBackground = Console.BackgroundColor;
+
+      Console.ForegroundColor = colorFoderground;
+      if (colorBackground >= 0)
+         Console.BackgroundColor = (ConsoleColor) colorBackground;
+
+      Console.Write (text);
+
+      Console.ForegroundColor = curColorForeground;
+      Console.BackgroundColor = curColorBackground;
+   }
+
+   // Notify about some exeption or give some feedback on console. The text will be dissapeared after 2 seconds
+   public static void Notify (string notification, ConsoleColor colorFoderground = ConsoleColor.White, ConsoleColor colorBackground = ConsoleColor.DarkRed)
+   {
+      var (x, y) = Console.GetCursorPosition ();
+      Console.CursorVisible = false;
+
+      WriteColorText (notification, colorFoderground, (int) colorBackground);
+      System.Threading.Thread.Sleep (2000);
+
+      Console.SetCursorPosition (x, y);
+      Console.Write (new string (' ', notification.Length));
+      Console.SetCursorPosition (x, y);
+      Console.CursorVisible = true;
+   }
+}
---
+++ Calculator/CalculatorLibrary/Log.cs
@@ -0,0 +1,81 @@
+﻿
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary;
+
+// Date of one calcualtion to store in log 
+public class CalcData
+{ 
+   public string Input { get; set; } // Input in format "operand1 opeartion operand2", i.e. "2 x 3"
+   public string Result { get; set; }
+
+   public CalcData (string input, string result)
+   {
+      Input = input;
+      Result = result;
+   }
+}
+
+public class Log
+{
+   public int UsedCount { get; set; } // Count of launched times
+
+   public List<CalcData> Calculations { get; set; } = new ();
+
+   readonly string nameLogFile = "log.json";
+
+   public void Load ()
+   {
+      try
+      {
+         using (StreamReader reader = new (nameLogFile))
+         {
+            string json = reader.ReadToEnd ();
+
+            Log? log = JsonConvert.DeserializeObject<Log> (json);
+            if (log != null)
+            {
+               UsedCount = log.UsedCount;
+               Calculations = log.Calculations;
+            }
+         }
+      }
+      catch (Exception)
+      {
+         // No information about previous calculations available
+      }
+   }
+
+   public void Save ()
+   {
+      try
+      {
+         string json = JsonConvert.SerializeObject (this, formatting: Formatting.Indented);
+         using (StreamWriter writer = File.CreateText (nameLogFile))
+         {
+            writer.Write (json);
+         }
+      }
+      catch (Exception)
+      {
+      }
+   }
+
+   public void AddInfo (string input, string result)
+   {
+      Calculations.Add (new (input, result));
+      Save ();
+   }
+
+   public void Delete (int index)
+   {
+      Calculations.RemoveAt (index);
+      Save ();
+   }
+
+   public void Reset ()
+   {
+      Calculations.Clear ();
+      Save ();
+   }
+}
